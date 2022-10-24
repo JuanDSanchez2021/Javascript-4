88cf33e76d3551b19dbfc9ccd2c1f242e0956475
JuanDSanchez2021 <92406360+JuanDSanchez2021@users.noreply.github.com>
2022-10-24 09:32:29 -0500

    Nuevo javascript 4

---
+++ js4.js
@@ -0,0 +1,10 @@
+document.write("<br/><br/>Actividad 2<br/>")
+
+horaInicial = Number(prompt("Ingrese la hora inicial (0-23):"))
+
+frecuencia = Number(prompt("Ingrese la frecuencia (1-12):"))
+
+document.write("Hora incial: ", horaInicial," (tipo de dato: ",typeof(horaInicial),")")
+document.write("<br/>Frecuencia: ", frecuencia," (tipo de dato: ",typeof(frecuencia),")")
+
+// Verifica si se va a saltar a otro día
