e11e8914f792c98bcbd6a6f35d7016f7c704f729
JuanDSanchez2021 <92406360+JuanDSanchez2021@users.noreply.github.com>
2022-10-28 19:09:03 -0500

    Se agrego la frecuencia tipo dato

--- js4.js
+++ js4.js
@@ -10,4 +10,5 @@ frecuencia = Number(prompt("Ingrese la frecuencia (1-12):"))
 document.write("Hora incial: ", horaInicial," (tipo de dato: ",typeof(horaInicial),")")
 document.write("<br/>Frecuencia: ", frecuencia," (tipo de dato: ",typeof(frecuencia),")")
 
+
 // Verifica si se va a saltar a otro día
